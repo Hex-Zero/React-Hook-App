0db298f2e6bd504aeaff84fc054900e2763fdcf5
Hex-Zero <hex0@live.com>
2019-07-18 20:39:12 +0100

    Updates

---
+++ precache-manifest.9823283e656d5ec87a52fdd4b6d8a697.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e3385a930499e1d5eb19e9f27dd8a4",
+    "url": "/React-Hook-App/index.html"
+  },
+  {
+    "revision": "896ee0748657e07e69e3",
+    "url": "/React-Hook-App/static/css/main.d99912d7.chunk.css"
+  },
+  {
+    "revision": "6805858285e9158b2e94",
+    "url": "/React-Hook-App/static/js/2.dff789eb.chunk.js"
+  },
+  {
+    "revision": "896ee0748657e07e69e3",
+    "url": "/React-Hook-App/static/js/main.ea2b62d6.chunk.js"
+  },
+  {
+    "revision": "467f4f473f9cc6a4dea4",
+    "url": "/React-Hook-App/static/js/runtime~main.2cfbcd16.js"
+  }
+]);
